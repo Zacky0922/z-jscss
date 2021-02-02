ab2ee0e488263f2903a49d7efe6579f4ddc057bb
Zacky <abcn00200@yahoo.co.jp>
2021-02-02 17:42:56 +0900

    fix

--- basic_variable/array.js
+++ basic_variable/array.js
@@ -3,7 +3,7 @@ let zArray = new (class {
     
     // 配列を転置する
     transpose(arr) {
-        let arr_ = [];
+        let arr_ = new Array(new Array());
         for (let i = 0; i < arr.length; i++){
             for (let j = 0; j < arr[i].length; j++){
                 arr_[j][i] = arr[i][j];
