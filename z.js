b5729cacc480a75057f1e0940dadfdbec201e94e
Zacky <abcn00200@yahoo.co.jp>
2021-08-25 16:25:09 +0900

    Update z.js

--- z.js
+++ z.js
@@ -7,7 +7,7 @@ export { zTable } from "./basic_components/table.js";
 // basic_variable
 export { zArray } from "./basic_variable/array.js";
 
-export { CSVtx2json } from "./document/CSVtx2json";
+export { CSVtx2json } from "./document/CSVtx2json.js";
 
 zDebug.addLog("z-js loaded");
 console.log("z.js loaded.")
